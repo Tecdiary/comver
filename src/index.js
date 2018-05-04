f09514c99be83089eefbb8df70a535e0b3e6178d
Mian Saleem <saleem@tecdiary.com>
2018-05-04 16:41:24 +0800

    Improve logs

--- src/index.js
+++ src/index.js
@@ -12,53 +12,51 @@ module.exports = args => new Promise((resolve) => {
             const libPkg = path.resolve('package.json');
             const cvPkg = require(libPkg);
 
-            console.log(colors.fg.Cyan, `
+            console.log(colors.fg.Cyan, colors.Bright, `
 ${cvPkg.name} -`, colors.fg.Magenta, `v${cvPkg.version}`, colors.Reset, `
 
 ${cvPkg.description}
 By: ${cvPkg.author}
 
-Help:`, colors.fg.Green, `
+Help:`, colors.fg.Green, colors.Bright, `
 
-  comver major  - to bump major version 0.0.0 to 1.0.0`, colors.fg.Cyan, `
-  comver minor  - to bump minor version 0.0.0 to 0.1.0`, colors.fg.Yellow, `
-  comver patch  - to bump patch version 0.0.0 to 0.0.1`, colors.Reset, `
+    comver major    `, colors.Reset, `to bump major version 0.0.0 to 1.0.0`, colors.fg.Cyan, colors.Bright, `
+    comver minor    `, colors.Reset, `to bump minor version 0.0.0 to 0.1.0`, colors.fg.Yellow, colors.Bright, `
+    comver patch    `, colors.Reset, `to bump patch version 0.0.0 to 0.0.1`, colors.Reset, `
 
 Please check https://semver.org/ to know more about Semantic Versioning.`);
 
         } else {
-            statusGit().then((res) => {
-                const composerJson = path.join(process.cwd(), 'composer.json');
-                fs.stat(composerJson, function(err, stat) {
-                    if(err == null) {
+            const composerJson = path.join(process.cwd(), 'composer.json');
+            fs.stat(composerJson, function(err, stat) {
+                if (err == null) {
+                    statusGit().then((res) => {
                         const pkg = require(composerJson);
                         const nextVersion = pkg.version ? getNextVersion(pkg.version, type) : '0.0.0';
                         pkg.version = nextVersion;
                         fs.writeFileSync(composerJson, JSON.stringify(pkg, null, '\t'));
-
                         try {
                             const packageJson = path.join(process.cwd(), 'package.json');
                             const nPkg = require(packageJson);
                             nPkg.version = nextVersion;
                             fs.writeFileSync(packageJson, JSON.stringify(nPkg, null, '\t'));
                         } catch (ex) {
-                            console.log(colors.fg.Yellow, `Error: `, ex);
+                            console.log(colors.fg.Yellow, `Unable to set package.json version: `, ex);
                         }
-
                         const tagName = `v${nextVersion}`;
                         createGitCommit(tagName).then(createGitTag).then(pushGitTag).then(tag => resolve(tag));
-                    } else if(err.code == 'ENOENT') {
-                        console.log(colors.fg.Red, `Current working directory doesn't have composer.json file.`, colors.Reset);
-                    } else {
-                        console.log(colors.fg.Red, `Error: `, err.code);
-                    }
-                });
-            }).catch((err) => {
-                console.log(colors.fg.Red, 'Working directory is not clean!', colors.Reset);
-                console.log(colors.fg.Yellow, 'Please commit your changes before versioning.', colors.Reset);
+                    }).catch((err) => {
+                        console.log(colors.fg.Red, 'Working directory is not clean!', colors.Reset);
+                        console.log(colors.fg.Yellow, 'Please commit your changes before versioning.', colors.Reset);
+                    });
+                } else if (err.code == 'ENOENT') {
+                    console.log(colors.fg.Red, `Current working directory (`, colors.fg.Yellow, process.cwd(), colors.fg.Red, `) doesn't have`, colors.fg.Cyan, `composer.json`, colors.fg.Red, `file.`, colors.Reset);
+                } else {
+                    console.log(colors.fg.Red, `Error: `, err.code);
+                }
             });
         }
     } else {
-        console.log(colors.fg.Reb, `Unknown arg!', colors.fg.Cyan, 'please type 'comver -h' for help.`);
+        console.log(colors.fg.Reb, `Unknown arg!`, colors.fg.Cyan, `please type 'comver -h' for help.`);
     }
 });
